f941f8999f0060e1ad8410138ae1757d66787b9f
Anas Ali <mynaameis321@gmail.com>
2024-10-05 14:50:00 +0500

    remove errors

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Library myLibrary = new Library();
-        myLibrary.addBook("Harry Potter");
-        myLibrary.addBook("Lord of the Rings");
+        myLibrary.addBokks("Harry Potter");
+        myLibrary.addBokks("Lord of the Rings");
         myLibrary.listBooks();
     }
 }
